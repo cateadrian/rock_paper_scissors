d4da615b8e652826cc45a2c689f9693024af1668
Adrian Cate <77905982+cateadrian@users.noreply.github.com>
2024-06-12 20:02:20 -0600

    Add playRound function

--- script.js
+++ script.js
@@ -24,3 +24,43 @@ function getHumanChoice(){
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice){
+    switch(humanChoice){
+        case "rock":
+            if (computerChoice == "rock"){
+                console.log("Its a tie!");
+            } else if (computerChoice == "paper"){
+                console.log(`You lose ${computerChoice} beats ${humanChoice}.`);
+                computerScore++;
+            } else if (computerChoice == "scissor"){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+            };
+            break;
+        case "paper":
+            if (computerChoice == "rock"){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+            } else if (computerChoice == "paper"){
+                console.log("Its a tie!");
+            } else if (computerChoice == "scissor"){
+                console.log(`You lose ${computerChoice} beats ${humanChoice}.`);
+                computerScore++;
+            };
+            break;
+        case "scissor":
+            if (computerChoice == "rock"){
+                console.log(`You lose ${computerChoice} beats ${humanChoice}.`);
+                computerScore++;
+            } else if (computerChoice == "paper"){
+                console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+                humanScore++;
+            } else if (computerChoice == "scissor"){
+                console.log("Its a tie!");
+            };
+            break;
+    };
+}
+
+
