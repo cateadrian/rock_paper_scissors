c79206b8a0e987ab06f9af72943959a74d8102ee
Adrian Cate <77905982+cateadrian@users.noreply.github.com>
2024-06-12 19:23:17 -0600

    Add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+function getComputerChoice(){
+    let randomNumber = Math.random().toFixed(2);
+    const oneThird = (1/3).toFixed(2);
+    const twoThirds = (2/3).toFixed(2);
+
+    if(randomNumber >= 0 && randomNumber < oneThird){
+        return "rock";
+    } else if(randomNumber >= oneThird && randomNumber < twoThirds){
+        return "paper";
+    } else {
+        return "scissor";
+    };
+}
